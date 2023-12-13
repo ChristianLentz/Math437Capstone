8d7c338559b8e689c59cc810ecbafcba22fb219b
ChristianLentz <cjlsoccer23@gmail.com>
2023-12-12 18:15:27 -0600

    outlining fourier code

--- Fourier.py
+++ Fourier.py
@@ -5,3 +5,17 @@ Transform.
 
 Author: Christian Lentz
 """
+
+class fourier: 
+    
+    def __init__(self): 
+        
+        """ 
+        A PDE solver for the wave equation using Fourier spectral methods. 
+        """
+    
+    def transform(self, vals): 
+        
+        """ 
+        Perform a Fourier transform on some vecotr using the Fourier matrix. 
+        """
